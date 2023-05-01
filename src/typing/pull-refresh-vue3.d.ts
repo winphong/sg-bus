7982b55af933d035e1ec2fbf1c17b109ded953b4
winphong <winphong@u.nus.edu>
2023-05-01 16:30:41 +0800

    add pull refresh on bus arrival timing view

---
+++ src/typing/pull-refresh-vue3.d.ts
@@ -0,0 +1,1 @@
+declare module 'pull-refresh-vue3'
--- src/views/BusTimingView.vue
+++ src/views/BusTimingView.vue
@@ -1,12 +1,14 @@
 
 <script lang="ts">
 import type { IBusArrival } from '@/model';
-import BusArrival from '@/components/BusArrival.vue';
+import BusArrivalListItem from '@/components/BusArrivalListItem.vue';
+import PullRefresh from 'pull-refresh-vue3'
+
 
 export default {
-    components: { BusArrival },
+    components: { BusArrivalListItem, PullRefresh },
     data() {
-        return { arrivals: [] as IBusArrival[] }
+        return { arrivals: [] as IBusArrival[], isLoading: false }
     },
     async mounted() {
         const data = (await this.$axios.get(`/timing/${this.$route.params.busStopId}`)).data
@@ -15,6 +17,19 @@ export default {
     computed: {
         busStop() {
             return this.$store.getters.getSelectedBusStop(this.$route.params.busStopId as string)
+        },
+        loading() {
+            return this.isLoading
+        }
+    },
+    methods: {
+        async onRefresh() {
+            this.isLoading = true
+
+            const data = (await this.$axios.get(`/timing/${this.$route.params.busStopId}`)).data
+            this.arrivals = data.Services
+
+            this.isLoading = false
         }
     }
 
@@ -22,13 +37,20 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div>{{ busStop.Description }} ({{ busStop.BusStopCode }})</div>
-    </div>
-    <div v-for="arrival in arrivals" :key="arrival.service_num">
-        <bus-arrival :bus-arrival="arrival"></bus-arrival>
-    </div>
+    <pull-refresh class="pull-container " v-model="loading" @refresh="onRefresh" pulling-text="Pull down to refresh"
+        loading-text="Loading..." loosing-text="Release to refresh" success-text="">
+        <div>
+            <div>{{ busStop.Description }} ({{ busStop.BusStopCode }})</div>
+        </div>
+        <div v-for="arrival in arrivals" :key="arrival.service_num">
+            <bus-arrival-list-item :bus-arrival="arrival"></bus-arrival-list-item>
+        </div>
+    </pull-refresh>
 </template>
   
-<style scoped></style>
+<style scoped>
+.pull-container {
+    height: 90vh;
+}
+</style>
   
